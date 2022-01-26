407f45df44f8a87c1a0133af7f703444b3fea6c5
Latekq <80835397+Latekq@users.noreply.github.com>
2022-01-26 17:31:01 +0100

    Add constructor with text

--- src/main/java/SampleAction.java
+++ src/main/java/SampleAction.java
@@ -2,8 +2,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class SampleAction extends AbstractAction {
+
     public SampleAction(String text, Icon icon) {
         super(text, icon);
+    }
+
+    public SampleAction(String text) {
+        super(text);
     }
 
     public void actionPerformed(ActionEvent e) {
